5a883d8149d05a8a59eb851bca586a1cd14068c0
pkuulme <peep.kuulme@itcollege.ee>
2017-03-20 20:05:32 +0200

    Changed initialization and tests

--- quiz.py
+++ quiz.py
@@ -5,12 +5,13 @@ from questions import Add, Multiply, Subtract, Divide
 
 
 class Quiz():
-    
+  
 
     def __init__(self):
         self.questions = []
         self.answers = []
         question_types = (Add, Subtract, Multiply)
+        
         # generate 10 random questions from number 1 to 20
         for _ in range(5):
             num1 = random.randint(1, 5)
@@ -23,8 +24,9 @@ class Quiz():
             self.questions.append(question)
 
     def take_quiz(self):
+        #log the start time
         self.start_time = datetime.datetime.now()
-
+        #ask all the questions
         for question in self.questions:
             self.answers.append(self.ask(question))
         else:
@@ -34,14 +36,16 @@ class Quiz():
 
     def ask(self, question):
         correct = False
+        #log the start time
         question_start = datetime.datetime.now()
-
+        #Capture the answer
         answer = input(question.text + ' = ')
+        #check the answer
         if answer == str(question.answer):
             correct = True
-
+        #log the end time
         question_end = datetime.datetime.now()
-
+        #Return the amount of correct answers and time it took
         return correct, question_end - question_start
 
     def total_correct(self):
@@ -52,12 +56,18 @@ class Quiz():
         return total
 
     def summary(self):
+        #Print the amount of correct answers
         print("You got {} out of {} right".format(
              self.total_correct(), len(self.questions)))
-
+        #Print the time it took to answer the questions
         print("It took you {} seconds".format((self.end_time - self.start_time).seconds))
 
-    @staticmethod
-    def start_quiz():
-        quiz = Quiz()
-        quiz.take_quiz()
+    #Initiates the program
+
+def start_quiz():
+    quiz = Quiz()
+    quiz.take_quiz()
+
+
+if __name__=='__main__':
+    start_quiz()
